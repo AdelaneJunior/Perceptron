fc9da16f66a1045f4de8b2f79adb9ed4e314a97d
Adelane Junior <adelane.junior@aluno.ueg.br>
2023-11-25 23:11:16 -0300

    Alteracoes semanticas e comentarios com descricoes

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import numpy as np
 from random import randint
 
 erro = 0
-iteracoes = 1
+ciclos = 1
 
 
+# vetor com valores de inciais para o treinamento
 def dados_entrada_treino():
     entrada_treino = [
         [1, 0, 0],
@@ -15,23 +16,25 @@ def dados_entrada_treino():
     return entrada_treino
 
 
+# iniciação dos neuronios com pesos aleatórios e os passando para treino
 def perceptron_treino(peso1, peso2, peso3):
-    global iteracoes
+    global ciclos
     global erro
-    if iteracoes == 1:
+    if ciclos == 1:
         print("começo")
-        print("pesos iniciais: ", peso1, " ", peso2, " ", peso3,)
+        print("pesos iniciais: ", peso1, " ", peso2, " ", peso3, )
     neuronios = [peso1, peso2, peso3]
     entrada_treino = dados_entrada_treino()
     neuronios = treino_trabalho_neuronios(entrada_treino, neuronios)
-    print("quantidade erros na", iteracoes,"iteração: ", erro)
+    print("cliclo", ciclos, "quantidade erros: ", erro)
     if erro > 0:
-        iteracoes = iteracoes + 1
+        ciclos = ciclos + 1
         erro = 0
         perceptron_treino(neuronios[0], neuronios[1], neuronios[2])
     return neuronios
 
 
+# entrada dos valores de treino e trabalho dos neuronios
 def treino_trabalho_neuronios(entrada_treino, neuronios):
     valores = np.empty(3)
     for i in range(0, 4):
@@ -43,74 +46,78 @@ def treino_trabalho_neuronios(entrada_treino, neuronios):
     return neuronios
 
 
-def verifica_resultado(valores, ciclo, neuronios, entrada):
+# validação do resultado obtido através dos neuronios conforme o ciclo em questão
+# e envio para manutenção nos pesos
+def verifica_resultado(valores, iteracao, neuronios, entrada):
     resultado = int(np.sum(valores))
     if resultado >= 1:
         resultado = 1
-    # print("resultado ciclo ", ciclo + 1, ": ", resultado)
 
-    if ciclo == 0:
+    # validação do resultado obtido conforme o ciclo em questão
+    if iteracao == 0:
         if resultado != 0:
-            return atualiza_peso_sub(ciclo, neuronios, entrada)
+            return atualiza_peso_sub(iteracao, neuronios, entrada)
         return neuronios
 
-    if ciclo == 1:
+    if iteracao == 1:
         if resultado != 1:
-            return atualiza_peso_sum(ciclo, neuronios, entrada)
+            return atualiza_peso_sum(iteracao, neuronios, entrada)
         return neuronios
 
-    if ciclo == 2:
+    if iteracao == 2:
         if resultado != 1:
-            return atualiza_peso_sum(ciclo, neuronios, entrada)
+            return atualiza_peso_sum(iteracao, neuronios, entrada)
         return neuronios
 
-    if ciclo == 3:
+    if iteracao == 3:
         if resultado != 0:
-            return atualiza_peso_sub(ciclo, neuronios, entrada)
+            return atualiza_peso_sub(iteracao, neuronios, entrada)
         return neuronios
 
 
-def atualiza_peso_sub(ciclo, neuronios, entrada):
+# manutenção dos pesos dos neuronios em caso de necessidade de subtração
+def atualiza_peso_sub(iteracao, neuronios, entrada):
     global erro
     erro += 1
     for x in range(0, 3):
         if neuronios[x] != 0:
-            neuronios[x] = neuronios[x] - entrada[ciclo][x]
+            neuronios[x] = neuronios[x] - entrada[iteracao][x]
     return neuronios
 
 
-def atualiza_peso_sum(ciclo, neuronios, entrada):
+# manutenção dos pesos dos neuronios em caso de necessidade de adição
+def atualiza_peso_sum(iteracao, neuronios, entrada):
     global erro
     erro += 1
     for x in range(0, 3):
         if neuronios[x] != 1:
-            neuronios[x] = neuronios[x] + entrada[ciclo][x]
+            neuronios[x] = neuronios[x] + entrada[iteracao][x]
     return neuronios
 
 
+# entrão os neuronios treinados e realizam seu trabalho
 def trabalho_neuronios(neuronios):
-    primeira_pergunta = input('Primeira pergunta: ')
-    entrada1 = 1 if primeira_pergunta == "sim" else 0
-    segunda_pergunta = input('Segunda pergunta: ')
-    entrada2 = 1 if segunda_pergunta == "sim" else 0
-    terceira_pergunta = input('Terceira pergunta: ')
-    entrada3 = 1 if terceira_pergunta == "sim" else 0
-    entrada = [entrada1, entrada2, entrada3]
-    print(entrada)
-    valores = np.empty(3)
-    for i in range(0, 3):
-        valores[i] = neuronios[i] * entrada[i]
-    resultado = int(np.sum(valores))
-    if resultado >= 1:
-        resultado = 1
-    print("resultado: ", resultado)
+    continuar = "sim"
+    while continuar != "nao":
+        primeira_pergunta = input('Primeira pergunta: ')
+        entrada1 = 1 if primeira_pergunta == "sim" else 0
+        segunda_pergunta = input('Segunda pergunta: ')
+        entrada2 = 1 if segunda_pergunta == "sim" else 0
+        terceira_pergunta = input('Terceira pergunta: ')
+        entrada3 = 1 if terceira_pergunta == "sim" else 0
+        entrada = [entrada1, entrada2, entrada3]
+        print(entrada)
+        valores = np.empty(3)
+        for i in range(0, 3):
+            valores[i] = neuronios[i] * entrada[i]
+        resultado = int(np.sum(valores))
+        if resultado >= 1:
+            resultado = 1
+        print("resultado: ", resultado)
+        continuar = input('continuar ? ')
 
 
 if __name__ == '__main__':
     neuronios_treinados = perceptron_treino(randint(0, 1), randint(0, 1), randint(0, 1))
-    # neuronios_treinados = perceptron_treino(0, 0, 0, 0)
-    continuar = "sim"
-    print("Foram necessários " + str(iteracoes) + " para completar o aprendizado")
-    while continuar != "nao":
-        trabalho_neuronios(neuronios_treinados)
-        continuar = input('continuar ? ')
+    print("Necessário(s) " + str(ciclos) + " ciclo(s) para completar o aprendizado")
+    trabalho_neuronios(neuronios_treinados)
